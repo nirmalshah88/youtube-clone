27d89f262f62b114d7b919474c5170fa483b6e8d
Nirmal Shah <nirmalshah2000@gmail.com>
2022-07-09 02:34:50 -0700

    Started building out <RecommendedVideos /> component with <VideoCard/> components within them.

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,9 +1,42 @@
 import React from "react";
 import "./RecommendedVideos.css";
+import VideoCard from "./VideoCard";
 
 function RecommendedVideos() {
   return (
-    <div className="recommendedVideos">This is where recommended videos go</div>
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="🔴 Let's Build a YouTube Clone with REACT JS for Beginners"
+          views="246621"
+          timestamp="3 days ago"
+          channelImage=""
+          channel="Clever Programmer"
+        />
+        <VideoCard
+          title="🔴 Let's Build a YouTube Clone with REACT JS for Beginners"
+          views="246621"
+          timestamp="3 days ago"
+          channelImage=""
+          channel="Clever Programmer"
+        />
+        <VideoCard
+          title="🔴 Let's Build a YouTube Clone with REACT JS for Beginners"
+          views="246621"
+          timestamp="3 days ago"
+          channelImage=""
+          channel="Clever Programmer"
+        />
+        <VideoCard
+          title="🔴 Let's Build a YouTube Clone with REACT JS for Beginners"
+          views="246621"
+          timestamp="3 days ago"
+          channelImage=""
+          channel="Clever Programmer"
+        />
+      </div>
+    </div>
   );
 }
 
