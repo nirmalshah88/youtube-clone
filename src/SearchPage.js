aa763472a04f90d3e665ccd4516be312e080694c
Nirmal Shah <nirmalshah2000@gmail.com>
2022-07-09 23:09:51 -0700

    Finished building out <ChannelRow /> and <VideoRow />'s for <SearchPage />. I should either add dummy data or pull in from YT API from my search results.

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import FilterIcon from "@mui/icons-material/TuneSharp";
 
 import ChannelRow from "./ChannelRow";
+import VideoRow from "./VideoRow";
 
 import "./SearchPage.css";
 
@@ -21,6 +22,18 @@ function SearchPage() {
         noOfVideos={382}
         description="You can find awesome programming tutorials here"
         verified
+      />
+
+      <hr />
+
+      <VideoRow
+        views="1.4M"
+        subs="659K"
+        description="Do you want a FREE one hour training... check this out"
+        timestamp="59 seconds ago"
+        channel="Clever Programmer"
+        title="Let's Build a YouTube Clone with REACT JS"
+        image="https://yt3.ggpht.com/ytc/AKedOLRbdv3Di8paQyrgMF_VwFXPkhwVzcW59Vgo8dTsyw=s48-c-k-c0x00ffffff-no-rj"
       />
     </div>
   );
--- src/VideoRow.js
+++ src/VideoRow.js
@@ -1,9 +1,32 @@
 import React from "react";
 
-import "./VideoRow";
+import "./VideoRow.css";
 
-function VideoRow() {
-  return <div className="videoRow">VideoRow</div>;
+function VideoRow({
+  views,
+  subs,
+  description,
+  timestamp,
+  channel,
+  title,
+  image,
+}) {
+  return (
+    <div className="videoRow">
+      <img className="videoRow__image" src={image} alt="" />
+      <div className="videoRow__text">
+        <h3>{title}</h3>
+        <p className="videoRow__headline">
+          {channel} •{" "}
+          <span className="videoRow__subs">
+            <span className="videoRow__subsNumber">{subs}</span> Subscribers
+          </span>{" "}
+          {views} views • {timestamp}
+        </p>
+        <p className="videoRow__description">{description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default VideoRow;
